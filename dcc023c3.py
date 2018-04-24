97c7dc4644f815a7065949ce35e558ffedd90c98
Victor Andres Caram Guimaraes <victor.caram@xingu.grad.dcc.ufmg.br>
2018-04-24 20:29:56 -0300

    Enquadramento totalmente funcional

--- dcc023c3.py
+++ dcc023c3.py
@@ -36,14 +36,17 @@ def enquadra(mensagem, id, flag):
     sincronizacao = "dc c0 23 c2"
 
     sincFinal = cvtHEX(sincronizacao)
-    print(sincFinal)
 
     if(id > 0):
         idFinal = cvtHEX("01")
     else:
         idFinal = cvtHEX("00")
 
-    length = cvtHEX(str(len(mensagem)))
+
+    lengthHEX = '{:04x}'.format(len(mensagem))
+    lengthHEX = ' '.join(lengthHEX[i:i+2] for i in range(0, len(lengthHEX), 2))
+    length = cvtHEX(lengthHEX)
+
 
     if(flag == flagACK):
         flagFinal = cvtHEX(flagACK)
@@ -51,42 +54,21 @@ def enquadra(mensagem, id, flag):
         flagFinal = cvtHEX(flagEnvio)
 
     chksum = cvtHEX("00 00")    
-
-
     # print(sincFinal, length, chksum, idFinal, flagFinal)
     enquadramento = sincFinal + sincFinal + length + chksum + idFinal + flagFinal
-    print(enquadramento)
+
     return enquadramento     
-
-    # read_data = '0100100001100101011011000110110001101111001011000010000001110111011011110111001001101100011001000010000100001010'
-    # read_data1 = 'hello, wolrd!'
-    # length = len(read_data)
-    # length_bin = format(length, '016b')
-    # checksum = format(0, '016b')
-    # id = format(0, '08b')
-    # flag = format(0, '08b')
-    # # enquadrado = sincronizacao +" "+ sincronizacao +" "+ length_bin +" "+ checksum +" "+ id +" "+ flag +" "+ read_data
-    # enquadrado = sincronizacao + sincronizacao + length_bin + checksum + id + flag + read_data
-    # #checksum
-    # data = "45 00 00 47 73 88 40 00 40 06 a2 c4 83 9f 0e 85 83 9f 0e a1"
-    # data = data.split()
-    # tam = len(data)
-    # data = map(lambda x: int(x,16), data)
-    # print(data)
-    # data = struct.pack("%dB" % tam, *data)
-
-    # print (' '.join('%02X' % ord(x) for x in data))
-    # print ("Checksum: 0x%04x" % checksum(data))
 
 #codificar
 def encode16(enquadrado):
-    codificado = "".join("{:02X}".format(int(n,2)) for n in enquadrado.split(' ') if n)
-    decode16(codificado)
+    codificado = "".join('%02x' %x  for x in enquadrado)
+    return codificado
 
 #decodificae
 def decode16(codificado):
-    decodificado = "".join("{:02X}".format(int(n,16)) for n in codificado.split(' ') if n)
-    print(decodificado)
+    codificado = ' '.join(codificado[i:i+2] for i in range(0, len(codificado), 2))
+    decodificado = cvtHEX(codificado)
+    return decodificado
 
 
 
@@ -98,16 +80,31 @@ def TransmiteDados(input, conn):
     while True:
         dados = file.read()
         enquadramento = enquadra(dados, idDeEnvio, flagEnvio)
-        print(enquadramento)
+        dadosHEX = "".join("{:02x}".format(c) for c in dados)
+        dadosHEX = ' '.join(dadosHEX[i:i+2] for i in range(0, len(dadosHEX), 2))
+        msgAux = enquadramento + cvtHEX(dadosHEX)
+        checksumHEX = "%04x" % checksum(msgAux)
+        checksumHEX = ' '.join(checksumHEX[i:i+2] for i in range(0, len(checksumHEX), 2))
+        checksumHEX = cvtHEX(checksumHEX)
+        print(msgAux)
+        msgFinal = str(encode16(msgAux))
+        print(msgFinal)
+
+        print("Transmitindo")
+        conn.send(struct.pack("!i", len(msgFinal)))
+        conn.send(struct.pack("!" + str(len(msgFinal)) + "s", msgFinal.encode("ascii")))
+
+
         return
-
-
-
-    print("Transmitindo")
 
 
 def RecebeDados(output, conn):
     print("Recebendo")
+    recebe_num = int(struct.unpack("!i", connection.recv(4))[0])
+    string_byte = struct.unpack("!" + str(recebe_num) + "s", connection.recv(recebe_num))[0]
+    print(string_byte)
+
+    return
 
     
 identificador = sys.argv[1]
@@ -128,9 +125,6 @@ if (identificador == "-c") :
     #Thread1 = thread...transmite dados()
     #Thread2 = thread...recebe dados()
     #thread start()
-    envia_num = "blabla"
-    conn.send(struct.pack("!i", len(envia_num)))
-    conn.send(struct.pack("!" + str(len(envia_num)) + "s", envia_num.encode("ascii")))
     TransmiteDados(input_file, conn)
 
     #-------------Fecha a conexao----------------------
@@ -158,9 +152,4 @@ elif (identificador == "-s") :
         connection, address = conn.accept()
         connection.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         connection.setsockopt(SOL_SOCKET, SO_RCVTIMEO, struct.pack('ll', 15, 0))
-        recebe_num = int(struct.unpack("!i", connection.recv(4))[0])
-        string_byte = struct.unpack("!" + str(recebe_num) + "s", connection.recv(recebe_num))[0]
-
-        print(string_byte.decode("ascii"))
-
-    
+        RecebeDados(output_file, connection) 
